f248495e3b1b2631b18921a473fc19342f17af75
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-03 13:03:15 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
