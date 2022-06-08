8d4a647987916794dc49076887d8bf19feb1c3da
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-08 05:20:39 -0700

    Adding two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,14 +4,15 @@
 /**
  * add - prints the sum
  * @a: print int a
- * #b: print int b
+ * @b: print int b
  *
  * Return: Always 0 (Success)
  */
 int add(int a, int b)
+
 {
-	
 	int sum;
+
 	sum = a + b;
 
 	return (sum);
