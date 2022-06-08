07be34c2604b3633ac3f202d67586168bcb4ed77
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-07 19:25:45 -0700

    Check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,22 +8,22 @@
  */
 int print_sign(int n)
 {
-	int return;
+	int y;
 
 	if (n<0)
 	{
-		return = -1;
+	       y = -1;
 		_putchar('-');
 	}
 	else if (n > 0)
 	{
-		return = 1;
+		y = 1;
 		_putchar('+');
 	}
 	else
 	{
-		return = 0;
+		y = 0;
 		_putchar('0');
 	}
-	return (return);
+	return (y);
 }
