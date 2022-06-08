1e820d902c63c454836a2ddcac8a976167b55ffc
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-08 02:26:41 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table- prints 9 times table
+ *
+ */
+void times_table(void);
+{
+	int x;
+	int y;
+	int num;
+
+	for (x = 0; x <= 9; x++)
+	{
+		num = x;
+		for (y = 0; y <= 9; y++)
+		{
+			_putchar((num * y) + '0');
+
+			if (y < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+			
+
