63923e854e41daf61f229fefc30ea2a76a85d36e
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-07 18:07:14 -0700

    10 times alphabet commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
+	int j;
 	char y;
 
-	int j;
-
-	for (j=0; j<10; j++)
+	for (j = 0; j<10; j++)
 	{
-
-
-          	for (y = 'a'; y<= 'z'; y++)
-		{
+		for (y = 'a'; y <= 'z'; y++)
 			_putchar(y);
-		}
-		_putchar('\n');
 	}
-
+	_putchar('\n');
 }
-
-	
