2a570f301afa2235baffa67dbec572116dc2181e
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-07 18:46:39 -0700

    Checking if it is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is an alphabet
+ * @c: for comparison purposes
+ *
+ * Return: 1 if c is an alphabet and 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c > 97 && c < 122)
+	{
+		else if (c > 65 && c < 90)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+}
