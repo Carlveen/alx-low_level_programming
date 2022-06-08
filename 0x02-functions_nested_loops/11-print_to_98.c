9c8eb367d4ca17ab718510d03c4e442db93e9cad
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-08 06:00:27 -0700

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,17 @@
 void print_to_98(int n)
 
 {
-	while (n <= 98)
+	while (n < 98)
 	{
 		printf("%i, ", n);
 		n++;
 	}
-	while (n >= 98)
+	while (n > 98)
 	{
 		printf("%i, ", n);
 		n--;
 	}
+	printf("98");
 	putchar('\n');
 }
 
