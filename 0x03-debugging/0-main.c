867a1b440ef3d4761ec637af1154c91ab54dde6f
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-09 02:49:39 -0700

    main.c commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests a function that prints if a number is postive or negative
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+With this project, we will be dealing with how to debug a code from flaws
