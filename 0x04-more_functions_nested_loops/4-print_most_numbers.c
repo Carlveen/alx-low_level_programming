1d30e8e052cafd5082732d566cbf6c78bd8b9007
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-10 11:30:14 -0700

    my commit on 0 to 9 with no 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@ void print_most_numbers(void)
 {
 	int j;
 
-	for (j = 0; j <= 9; j++)
+	for (j = 0; j < 10; j++)
 	{
-		if (j != 2 && j != 4)
+		if ((j != 2) && (j != 4))
 			_putchar((j % 10) + '0');
-		
 	}
 
 	_putchar('\n');
 }
+
