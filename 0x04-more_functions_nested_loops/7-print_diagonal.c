a1cb7c53aceee805c5067ac41829763b6cb33642
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-10 14:44:44 -0700

    Diagonal on terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_diagnonal - prints a diagonal on terminal
+ * print_diagonal - prints a diagonal on terminal
  * @n: the length of the diagonal
  *
  * Return: a diagonal
@@ -11,15 +11,14 @@ void print_diagonal(int n)
 	int k, j;
 
 	if (n <= 0)
-	_putchar('\n');
-	
+		_putchar('\n');
 	for (k = 0; k < n; k++)
+	{
+		for (j = 0; j < k; j++)
 		{
-			for (j = 0; j < k; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
