2f66c9044b944a0990366aa064811b251d043e65
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-14 10:21:22 -0700

    String in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: a string to be reversed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	while (s[x])
+		x++;
+
+	while (x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
