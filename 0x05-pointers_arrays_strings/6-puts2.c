6bf2c8ffb93224cb6dbfd0ad7346e11a3db1bb1f
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-14 10:59:26 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of string
+ * @str: a string param
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	str++;
+
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
