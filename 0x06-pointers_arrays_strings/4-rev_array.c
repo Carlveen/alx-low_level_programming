ee28dff01cbee3fdccaa29283e4f8e76031c2da1
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-16 07:36:56 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverses the elements of array
+ * @a: a parameter
+ * @n: number of characters
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	strrev(a, n);
+
+	return (0);
+}
