7875101e9c8b60524b4bec0aa2e360b841e90f59
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-06-16 06:55:52 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares strings
+ * @s1: string param
+ * @s2: string param
+ *
+ * Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+
+	return (s1);
+}
