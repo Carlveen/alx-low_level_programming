f3c2d243702e934cd84ff0878432dbe334397965
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-07-06 11:20:45 -0700

    Function to pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,16 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  *
- *  @op: The operator
- *  @f: The function associated
+ * @op: The operator
+ * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
