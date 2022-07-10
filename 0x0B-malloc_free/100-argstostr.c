fb1c5637b71e069bb4be1095e0c25753704c211f
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-07-10 10:19:09 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,77 +1,43 @@
-#include <stdlib.h>
 #include "main.h"
-
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
  *
- * Return: number of words
+ * Return: the args one line at a time
  */
-int count_word(char *s)
+
+char *argstostr(int ac, char **av)
 {
-	int flag, c, w;
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
-	flag = 0;
-	w = 0;
-
-	for (c = 0; s[c] != '\0'; c++)
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (a < ac)
 	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			flag = 1;
-			w++;
+			count++;
+			b++;
 		}
 	}
-
-	return (w);
-}
-/**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- *
- */
-char **strtow(char *str)
-{
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-
-	while (*(str + len))
-		len++;
-	words = count_word(str);
-	if (words == 0)
-		return (NULL);
-
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-	if (matrix == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
+		return (NULL);
 	}
-
-	matrix[k] = NULL;
-
-	return (matrix);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
 }
