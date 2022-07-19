abd501cfe3bcfb7046beb33ffca8dd8487d5ffb8
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-07-19 07:33:12 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * reverse_listint - define a function
+ * @head: a parameter
+ *
+ * Return: 0
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
