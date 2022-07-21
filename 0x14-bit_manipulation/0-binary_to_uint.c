dd23ced407045e64a9c2a322a021558906cb6a78
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-07-21 11:10:21 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_to_uint - converts a binary number to an
- * unsigned int.
+ * unsigned int
  * @b: binary.
  *
- * Return: unsigned int.
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
